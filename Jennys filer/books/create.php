023cf1f5e982a5706a0025c107dbd51146722737
Jenny Norén <littlestarjenny6@gmail.com>
2019-05-10 08:57:56 +0200

    la till min mapp

---
+++ Jennys filer/books/create.php
@@ -0,0 +1,32 @@
+<?php 
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, 
+Content-Type, Access-Control-Allow-Methods, Authorization- X-Requested-With');
+
+
+include_once '../config/conn.php';
+include_once '../classes/class.php';
+
+$database = new Database();
+$db = $database->connection();
+
+$book = new Books($db);
+
+$data = json_decode(file_get_contents("php://input"));
+
+$book->bookId = $book->bookId;
+$book->bookTitle = $book->bookTitle;
+$book->authorId = $book->authorId;
+
+if($book->createbook()) {
+    echo json_encode(
+        array('message' => 'Book Created')
+    );
+} else {
+    echo json_encode(
+        array('message' => 'Book Not Created')
+    );
+}
